d5bb6e7c27fab7db247704ca52f8f475007cc740
ViveIsAwesome <VIA@vive.website>
2016-05-07 22:32:57 +0200

    Added some code to add a test overlay

--- OpenVROverlayTest/Program.cs
+++ OpenVROverlayTest/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Valve.VR;
 
 namespace OpenVROverlayTest
 {
@@ -10,6 +10,39 @@ namespace OpenVROverlayTest
   {
     static void Main(string[] args)
     {
+      string ResourcePath = new FileInfo(Assembly.GetEntryAssembly().Location).Directory.FullName + "/Resources/";
+
+      // init
+      var error = EVRInitError.None;
+
+      OpenVR.Init(ref error);
+      if (error != EVRInitError.None) throw new Exception();
+
+      OpenVR.GetGenericInterface(OpenVR.IVRCompositor_Version, ref error);
+      if (error != EVRInitError.None) throw new Exception();
+
+      OpenVR.GetGenericInterface(OpenVR.IVROverlay_Version, ref error);
+      if (error != EVRInitError.None) throw new Exception();
+
+
+      // create overlay, ...
+      var overlay = OpenVR.Overlay;
+
+      ulong overlayHandle = 0, thumbnailHandle = 0;
+
+      overlay.CreateDashboardOverlay("overlayTest", "HL3", ref overlayHandle, ref thumbnailHandle);
+      overlay.SetOverlayFromFile(thumbnailHandle, $"{ResourcePath}/white-lambda.png");
+
+      overlay.SetOverlayWidthInMeters(overlayHandle, 2.5f);
+      overlay.SetOverlayInputMethod(overlayHandle, VROverlayInputMethod.Mouse);
+      Console.CancelKeyPress += (s, e) => overlay.DestroyOverlay(overlayHandle);
+
+      while (true)
+      {
+        Thread.Sleep(10); // This sucks but will do for now
+        if (overlay.IsOverlayVisible(overlayHandle))
+          overlay.SetOverlayFromFile(overlayHandle, $"{ResourcePath}/hl3.jpg");
+      }
     }
   }
 }
